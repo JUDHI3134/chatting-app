ac30a5589420ada5a845789aa2f3904eca53bde6
JUDHI3134 <judhistirbehera456@gmail.com>
2025-06-07 21:26:45 +0530

    add socket io for realtime chat

--- server/server.js
+++ server/server.js
@@ -5,10 +5,36 @@ import http from "http"
 import { connectDB } from "./lib/db.js"
 import userRouter from "./routes/userRoutes.js"
 import messageRouter from "./routes/messageRoutes.js"
+import { Server } from "socket.io"
 
 //create express app and HTTP server
 const app = express()
 const server = http.createServer(app)
+
+//initialized socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//store online user
+export const userSocketMap = {}; //{userId: socketId}
+
+//Socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId)
+
+    if (userId) userSocketMap[userId] = socket.id
+    
+    //emit online user to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
 
 //middleware setup
 app.use(express.json({ limit: "4mb" }))
